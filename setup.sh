548957f85d1ba6fd1646c259f7d8e675db593394
test <test@Golson-MBP.local>
2023-09-25 23:51:09 -0700

    check out oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sleep 5
-rm -rf $HOME/.z*
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.z* 
 
 # create symbolic link to dotfiles
+ln -s $(pwd)/ohmyzsh $HOME/.oh-my-zsh
 ln -s $(pwd)/zshrc $HOME/.zshrc
 ln -s $(pwd)/zshenv $HOME/.zshenv
 ln -s $(pwd)/zlogin $HOME/.zlogin
 ln -s $(pwd)/zprofile $HOME/.zprofile
 ln -s $(pwd)/zlogout $HOME/.zlogout
+
+source $HOME/.zshrc
