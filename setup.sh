2e266d5578454b7aaf306e82adc5c0f5accd3166
Golson Xie <oldfatcrab@gmail.com>
2023-10-02 21:33:30 -0700

    organize files

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ rm -rf $HOME/.oh-my-zsh
 rm -rf $HOME/.z* 
 
 # create symbolic link to dotfiles
-ln -s $PWD/ohmyzsh $HOME/.oh-my-zsh
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/zshenv $HOME/.zshenv
-ln -s $PWD/zlogin $HOME/.zlogin
-ln -s $PWD/zprofile $HOME/.zprofile
-ln -s $PWD/zlogout $HOME/.zlogout
-ln -s $PWD/p10k.zsh $HOME/.p10k.zsh
+ln -s $PWD/submodules/ohmyzsh $HOME/.oh-my-zsh
+ln -s $PWD/zsh/zshrc $HOME/.zshrc
+ln -s $PWD/zsh/zshenv $HOME/.zshenv
+ln -s $PWD/zsh/zlogin $HOME/.zlogin
+ln -s $PWD/zsh/zprofile $HOME/.zprofile
+ln -s $PWD/zsh/zlogout $HOME/.zlogout
+ln -s $PWD/zsh/p10k.zsh $HOME/.p10k.zsh
 
 # gitconfig
 git config --global core.pager delta
-git config --global include.path "$PWD/delta.gitconfig"
+git config --global include.path "$PWD/git/delta.gitconfig"
 
 source $HOME/.zshrc
