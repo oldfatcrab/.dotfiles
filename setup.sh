bd505eaa5f48f16eea798d6a347b38fa0a7287fa
test <test@golson-mbp.lan>
2023-10-02 22:23:39 -0700

    backup old files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+# Get timestammp
+CURRENT_TIME=$(date '+%Y_%m_%d_%H_%M_%S')
+
 # Brew installations
 
-# TODO: should create backup
-rm -rf $HOME/.oh-my-zsh
-rm -rf $HOME/.z* 
+# backup old configuration files
+if [ -d $HOME/.oh-my-zsh ]; then cp -r $HOME/.oh-my-zsh "$HOME/.oh-my-zsh_backup_$CURRENT_TIME" && rm -rf $HOME/.oh-my-zsh; fi
+if [ -e $HOME/.zshrc ]; then cp $HOME/.zshrc "$HOME/.zshrc_backup_$CURRENT_TIME" && rm $HOME/.zshrc; fi
+if [ -e $HOME/.zshenv ]; then cp $HOME/.zshenv "$HOME/.zshenv_backup_$CURRENT_TIME" && rm $HOME/.zshenv; fi
+if [ -e $HOME/.zlogin ]; then cp $HOME/.zlogin "$HOME/.zlogin_backup_$CURRENT_TIME" && rm $HOME/.zlogin; fi
+if [ -e $HOME/.zprofile ]; then cp $HOME/.zprofile "$HOME/.zprofile_backup_$CURRENT_TIME" && rm $HOME/.zprofile; fi
+if [ -e $HOME/.zlogout ]; then cp $HOME/.zlogout "$HOME/.zlogout_backup_$CURRENT_TIME" && rm $HOME/.zlogout; fi
+if [ -e $HOME/.p10k.zsh ]; then cp $HOME/.p10k.zsh "$HOME/.p10k.zsh_backup_$CURRENT_TIME" && rm $HOME/.p10k.zsh; fi
+if [ -e $HOME/.gitconfig ]; then cp $HOME/.gitconfig "$HOME/.gitconfig_backup_$CURRENT_TIME" && rm $HOME/.gitconfig; fi
 
 # create symbolic link to dotfiles
 ln -s $PWD/submodules/ohmyzsh $HOME/.oh-my-zsh
